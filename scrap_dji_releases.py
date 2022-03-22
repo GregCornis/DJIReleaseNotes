b8d223d44b22e5171fb9a42eb6afca9d29d6c650
Gregoire <gregoire.chalony@sites.fr>
2022-03-22 09:37:40 +0100

    Update with V5 release

--- scrap_dji_releases.py
+++ scrap_dji_releases.py
@@ -22,14 +22,15 @@ def get_download_urls():
     content = BeautifulSoup(r.content, "html.parser")
     links = content.find_all("a")
     release_divs = [link for link in links if link.string == "Release notes"]
-    assert(len(release_divs) == 2)
-    ios_div, android_div = release_divs
-    return Link.from_div(ios_div), Link.from_div(android_div)
+    assert(len(release_divs) == 3)
+    ios_div, android_div, android_v5_div = release_divs
+    return Link.from_div(ios_div), Link.from_div(android_div), Link.from_div(android_v5_div)
 
 if __name__ == "__main__":
-    ios, android = get_download_urls()
+    ios, android, v5 = get_download_urls()
     text = f"""iOS: {ios.name}
-Android: {android.name}"""
+Android: {android.name}
+V5: {v5.name}"""
     with open(RELEASE_TXT_PATH, "r") as input:
         content = input.read()
     if content == text:
